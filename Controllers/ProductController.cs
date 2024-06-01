ad930aff85354b22487ab470bf1d2da40c091058
Taqwa Kmail <taqwa.ykmail@gmail.com>
2024-06-01 10:56:03 -0700

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using ElectronicShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ElectronicContext context { get; set; }
+
+        public ProductController(ElectronicContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            string? usrEmail = HttpContext.Session.GetString("userEmail");
+            if (HttpContext.Session.GetString("userEmail") != null)
+            {
+                if(usrEmail == "admin@g.com")
+                {
+                    var product = context.Products.Find(id);
+                    context.Products.Remove(product);
+                    context.SaveChanges();
+                }
+            }
+               return RedirectToAction("Index","Home");
+        }
+    }
+}
